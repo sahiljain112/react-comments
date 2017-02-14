32aa35b26faca57996e6dd580f9933d40bfbf151
sahiljain112 <sahiljain112@gmail.com>
2017-02-14 18:32:24 +0530

    Sentence generate bug fix

--- src/comments.js
+++ src/comments.js
@@ -1,13 +1,13 @@
 import faker from 'faker'
 
-const comments = ( NUMBER_OF_COMMENTS ) => {
+const commentGenerator = ( NUMBER_OF_COMMENTS ) => {
   let i, comments = [];
   for(i = 0 ; i < NUMBER_OF_COMMENTS; i++)
-    comments.push(faker.lorem.sentence)
+    comments.push(faker.lorem.sentence())
   return comments
 }
 
-export const TEN_COMMENTS = comments(10)
-export const TWENTY_COMMETS = comments(20)
-export const FIVE_COMMENTS = comments(5)
-export const TWO_COMMENTS = comments(2)
+export const TEN_COMMENTS = commentGenerator(10)
+export const TWENTY_COMMETS = commentGenerator(20)
+export const FIVE_COMMENTS = commentGenerator(5)
+export const TWO_COMMENTS = commentGenerator(2)
